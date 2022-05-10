f4a55c61240e1404ab2f2656158f5da70c15ee1c
3 <vsirjdcrfz@mail.ru>
2022-05-10 13:21:57 +0400

    урок 4-4 -многопоточность.

---
+++ src/main/java/less/Less4_4_multithreading.java
@@ -0,0 +1,39 @@
+package less;
+
+public class Less4_4_multithreading {
+    static volatile char sim = 'A';
+    static Object mon = new Object();
+
+    static class WaitNotifyClass implements Runnable {
+        private char currentLetter;
+        private char nextLetter;
+
+        public WaitNotifyClass(char currentLetter, char nextLetter) {
+            this.currentLetter = currentLetter;
+            this.nextLetter = nextLetter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                synchronized (mon) {
+                    try {
+                        while (sim != currentLetter)
+                            mon.wait();
+                        System.out.print(currentLetter);
+                        sim = nextLetter;
+                        mon.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new Thread(new WaitNotifyClass('A', 'B')).start();
+        new Thread(new WaitNotifyClass('B', 'C')).start();
+        new Thread(new WaitNotifyClass('C', 'A')).start();
+    }   
+
+}
